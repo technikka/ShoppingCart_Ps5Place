d97717ce6d961d0b40fe9d54aed2a023b5aaaa73
Pythonikk <nikkibrusoe@gmail.com>
2022-12-24 11:59:34 -0800

    set up routing and created Navbar component with links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import "./styles/app.css";
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <Landing />
     </div>
   )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import {Link} from "react-router-dom";
+import "../styles/navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/shop">Shop</Link>
+      <Link to="/cart">Cart</Link>
+      
+    </div>
+  )
+}
+
+export default Navbar
