271d2b2d5b8afe04c0619e634b081bce9521ec57
Pythonikk <nikkibrusoe@gmail.com>
2022-12-27 13:06:25 -0800

    change shop to ps5 place

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,10 @@
 import "../styles/landing.css";
-// import AbstractArt from "../images/landing-background"
 
 const Landing = () => {
   return (
     <div className="landing">
       <div className="title">
-        <h1>Canvas Den</h1>
+        <h1>PS5 Place</h1>
       </div>
     </div>
   );
