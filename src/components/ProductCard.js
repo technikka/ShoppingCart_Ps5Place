2377c82e8c19acd9c2147dbb71aad8341dd22a67
Pythonikk <nikkibrusoe@gmail.com>
2022-12-29 10:59:07 -0800

    add validation and error/success messages for quantity input

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,6 +6,7 @@ import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
 const ProductCard = (props) => {
   const [quantity, setQuantity] = useState(1);
   const [showSuccessMsg, setShowSuccessMsg] = useState(false);
+  const [showErrorMsg, setShowErrorMsg] = useState(false);
 
   const quantityRef = useRef(quantity);
 
@@ -22,28 +23,51 @@ const ProductCard = (props) => {
   };
 
   const handleInput = (event) => {
-    setQuantity(parseInt(event.target.value));
+    setShowErrorMsg(false);
+    let value = event.target.value;
+    if (value) {
+      setQuantity(parseInt(value));
+    }
+  };
+
+  const quantityValid = () => {
+    if ([1, 2, 3, 4, 5, 6, 7].includes(quantity)) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
   const handleAddToCart = () => {
+    if (!quantityValid()) {
+      setShowSuccessMsg(false);
+      setShowErrorMsg(true);
+      return;
+    }
     quantityRef.current = quantity;
     setShowSuccessMsg(true);
     props.addToCart(props.product, quantity);
-  }
+  };
 
   const addToCartSuccessMsg = () => {
     let message;
     if (quantityRef.current === 1) {
-      message = "1 item was added to your cart."
+      message = "1 item was added to your cart.";
     } else {
-      message = `${quantityRef.current} items were added to your cart `
+      message = `${quantityRef.current} items were added to your cart `;
     }
-    return (
-      <div className="cart-success-msg">
-        {message}
-      </div>
-    )
-  }
+    return <div className="cart-success-msg">{message}</div>;
+  };
+
+  const quantityErrorMsg = () => {
+    let message;
+    if (quantity < 1) {
+      message = "You must select at least 1 to add to your cart.";
+    } else if (quantity > 7) {
+      message = "I'm sorry, we limit 7 copies per customer.";
+    }
+    return <div className="cart-error-msg">{message}</div>;
+  };
 
   return (
     <div className="product-card">
@@ -55,13 +79,27 @@ const ProductCard = (props) => {
         <button onClick={decrementQuantity} className="decrement">
           <FontAwesomeIcon icon={faAngleDown} />
         </button>
-        <input onChange={handleInput} type="text" value={quantity} size="1" />
+
+        <input
+          id="quantity"
+          onChange={handleInput}
+          type="number"
+          required
+          min="1"
+          max="7"
+          value={quantity}
+          size="1"
+        />
+
         <button onClick={incrementQuantity} className="increment">
           <FontAwesomeIcon icon={faAngleUp} />
         </button>
       </div>
-      {showSuccessMsg === true && (addToCartSuccessMsg())}
-      <button onClick={handleAddToCart} >Add To Cart</button>
+
+      {showSuccessMsg === true && addToCartSuccessMsg()}
+      {showErrorMsg === true && quantityErrorMsg()}
+
+      <button onClick={handleAddToCart}>Add To Cart</button>
       <div className="description">{props.product.description}</div>
     </div>
   );
