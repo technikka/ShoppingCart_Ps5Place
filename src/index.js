c707bcd9b35522995ffcf822882c395cc709bc29
Pythonikk <nikkibrusoe@gmail.com>
2023-01-03 16:57:53 -0800

    remove client-side routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouteSwitch from "./RouteSwitch";
+import Shop from "./components/Shop";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouteSwitch />
+    <Shop />
   </React.StrictMode>
 );
 
