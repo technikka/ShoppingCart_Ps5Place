4d2cd1848e49ddabf513c4c8792e036590d1219e
Pythonikk <nikkibrusoe@gmail.com>
2023-01-03 16:59:14 -0800

    remove reference to RouteSwitch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import RouteSwitch from "./RouteSwitch";
 import Shop from "./components/Shop";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
